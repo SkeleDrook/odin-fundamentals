fd397bd2e0aa709d69e62326bfae5f6ed01d94ab
SkeleDrook <60257404+SkeleDrook@users.noreply.github.com>
2023-03-18 19:46:43 +0100

    Rewrite functions into arrow functions

--- fundamentals3/index.js
+++ fundamentals3/index.js
@@ -8,3 +8,22 @@ function pow(x, n) {
 		x = x * base;
 	}
 }
+
+let min = (a, b) => (a < b) ? a : b;
+
+// function ask(question, yes, no) {
+// 	if (confirm(question)) yes();
+// 	else no();
+// }
+
+// ask(
+// 	"Do you agree?",
+// 	function () { alert("You agreed."); },
+// 	function () { alert("You canceled the execution."); }
+// );
+
+let ask = (question, yes, no) => confirm(question) ? yes() : no(); // not sure this one is correct
+
+ask(
+	"do you agree?", () => alert("You agreed."), () => alert("You canceled the execution.")
+)
